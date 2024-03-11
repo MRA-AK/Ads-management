40ee71d9ab449faa4acb7a232b7343c010f80b1f
MRA-AK <mra1373@gmail.com>
2024-03-11 12:49:20 +0100

    feat: use PostgreSQL as database

--- core/settings.py
+++ core/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
