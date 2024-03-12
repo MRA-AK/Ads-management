cb6d69efcce49d1405a4d21838ab4ea9ebc59f9e
MRA-AK <mra1373@gmail.com>
2024-03-12 11:29:04 +0100

    feat(api): user management api endpoints - JWT authentication - Create a new user - Get a list of users (Only for admins) - Get/Update/Delete user instance

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,30 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ADs management",
+        default_version="v1",
+        description="A platform where users can post advertisements and comment on other people's ads.",
+        terms_of_service="",
+        contact=openapi.Contact(email="mra1373@gmail.com"),
+        # license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # API endpoints
+    path("users/api/v1/", include("users.api.v1.urls")),
 ]
+
+# Swagger API documentation
+urlpatterns.append(path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger"))
+# Redoc API documentation
+urlpatterns.append(path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="redoc"))
